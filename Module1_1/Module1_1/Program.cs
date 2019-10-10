05e27c885364483d3f9d467d325875c0dc4dde75
antrimplato <antrimplato@gmail.com>
2019-10-10 16:38:53 +0300

    New project was created. Task 1 was completed.

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            a += b;
+            b = a - b;
+            a -= b;
+        }
+    }
+}
