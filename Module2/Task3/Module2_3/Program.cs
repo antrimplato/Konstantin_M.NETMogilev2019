3cde50945ca4e23f375f5bb298b4ad9585d3d64d
Antrimplato <antrimplato@gmail.com>
2019-10-16 22:50:10 +0200

    Module2_3
    Task 3 were completed.

---
+++ Module2/Task3/Module2_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Module2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            float a = CheckInput(Console.ReadLine());
+            Console.Write("Enter number B: ");
+            float b = CheckInput(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Your numbers: ");
+            Console.Write("A = " + a + " ");
+            Console.WriteLine("B = " + b + " ");
+
+            a += b;             b = a - b;             a -= b;
+
+            Console.WriteLine("Swapped numbers: ");
+            Console.Write("A = " + a + " ");
+            Console.WriteLine("B = " + b + " ");
+        }
+
+        //validation of the entered number
+        static float CheckInput(string x)  
+        {
+            bool b = float.TryParse(x, out float f);
+            if (!b)
+            {
+                Console.Write("Enter the correct number (use ',' as a delimiter): ");
+                string n = Console.ReadLine();
+                return CheckInput(n);
+            }
+            else
+            return f;
+        }
+
+    }
+}
