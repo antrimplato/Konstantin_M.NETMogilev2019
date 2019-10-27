c68994b1f5eed8776cb023b3f3929d914f238868
Antrimplato <antrimplato@gmail.com>
2019-10-27 18:34:56 +0300

    Module3_8_2
    Task8_2 comleted (spiral array)

---
+++ Module3/Task8_2/Module3_8_2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Module3_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array: ");
+            int arraySize = CheckInput(Console.ReadLine());
+
+            GetSpiral(arraySize);
+            Console.ReadKey();
+        }
+
+        public static void GetSpiral(int arraySize)
+        {
+            int[,] array = new int[arraySize, arraySize];
+            int row = 0;
+            int column = 0;
+            int dx = 1;
+            int dy = 0;
+            int turn = 0;
+            int calls = arraySize;
+            int temp = 0;
+
+            for (int counter = 0; counter < array.Length; counter++)
+            {
+                array[row, column] = counter + 1;
+                if ( --calls == 0 )
+                {
+                    calls = ((turn % 2) + (turn + 1) % 2) - (turn / 2 - 1) * arraySize - 2;
+                    temp = dx;
+                    dx = -dy;
+                    dy = temp;
+                    turn++;
+                }
+                row += dy;
+                column += dx;
+            }
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                for (int j = 0; j < arraySize; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        //validation of the entered integer
+        static int CheckInput(string inp)
+        {
+            if (int.TryParse(inp, out int parsedInp) && (parsedInp > 0))
+            {
+                return parsedInp;
+            }
+
+            else
+            {
+                Console.Write("Enter the correct size: ");
+                string correctInp = Console.ReadLine();
+                return CheckInput(correctInp);
+            }
+        }
+    }
+}
